4987d9bb752d915813a28890a450300815e54c29
Marco Behnke <marco@behnke.biz>
2019-01-26 18:14:05 +0100

    add timer

---
+++ Assets/Scripts/UI/UITimer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UITimer : MonoBehaviour
+{
+    public string NormalColor;
+    public string WarningColor;
+    public string DangerColor;
+
+    public float WarningLimit = 59;
+    public float DangerLimit = 20;
+    
+    public float TimeInSeconds;
+
+    public  bool _isRunning;
+    private Action _callback;
+
+    public TextMeshPro TimerDisplay;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isRunning)
+        {
+            TimeInSeconds -= Time.deltaTime;
+            
+            TimeSpan time = TimeSpan.FromSeconds(TimeInSeconds);
+            string str = time .ToString(@"mm\:ss");
+            TimerDisplay.text = str;
+        }
+
+        if (TimeInSeconds < DangerLimit)
+        {
+            Color color;
+            ColorUtility.TryParseHtmlString(DangerColor, out color);
+            TimerDisplay.color = color;
+        } else if (TimeInSeconds < WarningLimit)
+        {
+            Color color;
+            ColorUtility.TryParseHtmlString(WarningColor, out color);
+            TimerDisplay.color = color;
+        }
+
+        if (TimeInSeconds < 0)
+        {
+            _isRunning = false;
+
+            if (_callback != null)
+            {
+                _callback();
+            }
+        }
+    }
+
+    public void SetRunning(float timeInSeconds, Action callback)
+    {
+        TimeInSeconds = timeInSeconds;
+        _isRunning = true;
+    }
+}
