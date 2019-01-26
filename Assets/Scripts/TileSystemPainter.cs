3f37dbfacec3d51b96ae2a3a5be6680295999d85
juliushartmann <julius.hartmann@innogames.com>
2019-01-26 09:14:08 +0100

    Beginning of Tielsytem

---
+++ Assets/Scripts/TileSystemPainter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSystemPainter : MonoBehaviour
+{
+
+    [SerializedField] private GameObject _tilePrefab;
+
+    public void DrawTile(int index, float size)
+    {
+        GameObject tile = Instatiate(_tilePrefab);
+        tile.GetComponent<Image>().fillAmount =  size / 360; 
+        tile.rotate(index * size);
+    }
+
+}
