da1bae83919e959646d0b3a4072d052bc86c72fc
Aaron-Jonas Kuehne <aaron-jonas.kuehne@innogames.com>
2019-01-26 09:41:26 +0100

    fix tile painter script issues

--- Assets/Scripts/TileSystemPainter.cs
+++ Assets/Scripts/TileSystemPainter.cs
@@ -1,17 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class TileSystemPainter : MonoBehaviour
 {
+	[SerializeField] private GameObject _tilePrefab;
 
-    [SerializedField] private GameObject _tilePrefab;
-
-    public void DrawTile(int index, float size)
-    {
-        GameObject tile = Instatiate(_tilePrefab);
-        tile.GetComponent<Image>().fillAmount =  size / 360; 
-        tile.rotate(index * size);
-    }
-
+	public void DrawTile(int index, float size)
+	{
+		GameObject tile = Instantiate(_tilePrefab);
+		tile.GetComponent<Image>().fillAmount = size / 360;
+		tile.transform.Rotate(0, 0, index * size);
+	}
 }
