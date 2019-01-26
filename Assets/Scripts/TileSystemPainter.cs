0d91c72c908af1d40c7cf79e81fe932a48eb0de4
juliushartmann <julius.hartmann@innogames.com>
2019-01-26 10:13:54 +0100

    tile prefab and painter

--- Assets/Scripts/TileSystemPainter.cs
+++ Assets/Scripts/TileSystemPainter.cs
@@ -1,14 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Numerics;
+using UnityEngine;
 using UnityEngine.UI;
+using Vector3 = UnityEngine.Vector3;
 
 public class TileSystemPainter : MonoBehaviour
 {
 	[SerializeField] private GameObject _tilePrefab;
 
-	public void DrawTile(int index, float size)
+	public void DrawTile(int level, int index, float size)
 	{
 		GameObject tile = Instantiate(_tilePrefab);
 		tile.GetComponent<Image>().fillAmount = size / 360;
 		tile.transform.Rotate(0, 0, index * size);
+		tile.transform.localScale *= level;
 	}
 }
