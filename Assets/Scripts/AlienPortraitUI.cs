14b0db3e73185f40ca0381f187f00f95536515a2
Marco Behnke <marco@behnke.biz>
2019-01-26 21:15:41 +0100

    aspect ratio fix

---
+++ Assets/Scripts/AlienPortraitUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlienPortraitUI : MonoBehaviour
+{
+    private Image portrait;
+    private Image element_1;
+    private Image element_2;
+    
+    public void AddAlien(AlienContainer alien)
+    {
+        portrait.sprite = alien.AlienImage.sprite;
+        element_1.sprite = alien.ElementImage.sprite;
+    }
+}
