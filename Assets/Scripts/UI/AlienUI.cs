520fa314e55f0ffba20341f01cc08ade23db8255
Marco Behnke <marco@behnke.biz>
2019-01-26 18:24:19 +0100

    add remove and reset alien ui

--- Assets/Scripts/UI/AlienUI.cs
+++ Assets/Scripts/UI/AlienUI.cs
@@ -1,13 +1,39 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
 
 public class AlienUI : MonoBehaviour
 {
     public AlienUIElement ElementPrefab;
     private RectTransform _rectTransform;
 
+    private Dictionary<Elements, GameObject> _aliens;
+
     private void Start()
     {
         _rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void Reset()
+    {
+        foreach (var obj in _aliens)
+        {
+            Destroy(obj.Value);
+        }
+        
+        _aliens.Clear();
+    }
+
+    public void RemoveAlien(AlienContainer alien)
+    {
+        GameObject uiAlien;
+        _aliens.TryGetValue(alien.Element, out uiAlien);
+
+        if (uiAlien)
+        {
+            Destroy(uiAlien);
+            _aliens.Remove(alien.Element);
+        }
     }
 
     public void AddAliens(AlienContainer[] aliens)
@@ -19,7 +45,8 @@ public class AlienUI : MonoBehaviour
             RectTransform alientRect = alienUI.GetComponent<RectTransform>();
             alientRect.parent = _rectTransform;
             alientRect.localScale = Vector3.one;
+            
+            _aliens.Add(alien.Element, alientRect.gameObject);
         }
-
     }
 }
