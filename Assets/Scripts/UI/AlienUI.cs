31771b4591eb4598bd7634af0a8efe44715da78b
Marco Behnke <marco@behnke.biz>
2019-01-26 12:29:11 +0100

    fix error

--- Assets/Scripts/UI/AlienUI.cs
+++ Assets/Scripts/UI/AlienUI.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class AlienUI : MonoBehaviour
 {
+    public AlienUIElement ElementPrefab;
+    
     public void AddAliens(AlienContainer aliens)
     {
+        
         
     }
 }
--- Assets/Scripts/UI/AlienUIElement.cs
+++ Assets/Scripts/UI/AlienUIElement.cs
@@ -4,5 +4,5 @@ using UnityEngine;
 
 public class AlienUIElement : MonoBehaviour
 {
-    public 
+     
 }
