c3d12cf3744740a6d2e23ffe583c859e39a7a673
juliushartmann <julius.hartmann@innogames.com>
2019-01-26 10:33:32 +0100

    TileSystemPainter prefab added

--- Assets/Scripts/TileSystemPainter.cs
+++ Assets/Scripts/TileSystemPainter.cs
@@ -1,4 +1,6 @@
-﻿using System.Numerics;
+﻿using System.Collections.Generic;
+using System.Numerics;
+using DefaultNamespace;
 using UnityEngine;
 using UnityEngine.UI;
 using Vector3 = UnityEngine.Vector3;
@@ -6,10 +8,21 @@ using Vector3 = UnityEngine.Vector3;
 public class TileSystemPainter : MonoBehaviour
 {
 	[SerializeField] private GameObject _tilePrefab;
+//	[SerializeField] private Transform _tileContainer;
+	
+	private List<GameObject> gameObjects; 
+	private void Start()
+	{
+		gameObjects = new List<GameObject>();
+		for (int i = 1; i <= GlobalConfig.PlanetLevelHeight; i++)
+		{
+			gameObjects.Add(Instantiate(new GameObject("Level" + i), transform));
+		}
+	}
 
 	public void DrawTile(int level, int index, float size)
 	{
-		GameObject tile = Instantiate(_tilePrefab);
+		GameObject tile = Instantiate(_tilePrefab, gameObjects[level].transform);
 		tile.GetComponent<Image>().fillAmount = size / 360;
 		tile.transform.Rotate(0, 0, index * size);
 		tile.transform.localScale *= level;
