34f8c9b5a27678182442d1002c8f1fc3815017d4
juliushartmann <julius.hartmann@innogames.com>
2019-01-26 11:32:03 +0100

    finnished tileSystemPainter

--- Assets/Scripts/TileSystem.cs
+++ Assets/Scripts/TileSystem.cs
@@ -2,104 +2,114 @@
 using System.Collections.Generic;
 using DefaultNamespace;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlanetCycle
 {
-	private readonly List<PlanetRing> _rings = new List<PlanetRing>();
+    private readonly List<PlanetRing> _rings = new List<PlanetRing>();
 
-	public PlanetCycle()
-	{
-		for (int i = GlobalConfig.PlanetLevelHeight; i > 0; i--)
-		{
-			_rings.Add(new PlanetRing(i));
-		}
-	}
+    public PlanetCycle()
+    {
+        for (int i = GlobalConfig.PlanetLevelHeight; i > 0; i--)
+        {
+            _rings.Add(new PlanetRing(i));
+        }
+    }
 
-	public PlanetPiece GetFirstFreeAtAngle(float angle)
-	{
-		PlanetPiece lastPlanetPiece = null; //TODO think!
-		for (var index = 0; index < _rings.Count; index++)
-		{
-			var planetRing = _rings[index];
-			PlanetPiece piece = planetRing.GetAtAngle(angle);
-			if (piece.element != Elements.NotSet)
-			{
-				return lastPlanetPiece;
-			}
-			else if (index == _rings.Count - 1)
-			{
-				return piece;
-			}
+    public PlanetPiece GetFirstFreeAtAngle(float angle)
+    {
+        PlanetPiece lastPlanetPiece = null; //TODO think!
+        for (var index = 0; index < _rings.Count; index++)
+        {
+            var planetRing = _rings[index];
+            PlanetPiece piece = planetRing.GetAtAngle(angle);
+            if (piece.element != Elements.NotSet)
+            {
+                return lastPlanetPiece;
+            }
+            else if (index == _rings.Count - 1)
+            {
+                return piece;
+            }
 
-			lastPlanetPiece = piece;
-		}
+            lastPlanetPiece = piece;
+        }
 
-		throw new Exception("We did something wrong in the TileSystem list organization....");
-	}
+        throw new Exception("We did something wrong in the TileSystem list organization....");
+    }
 }
 
 class PlanetRing
 {
-	private readonly int _level;
-	private readonly float _anglePerPieceOnThisLevel;
-	private readonly List<PlanetPiece> _pieces;
+    private readonly int _level;
+    private readonly float _anglePerPieceOnThisLevel;
+    private readonly List<PlanetPiece> _pieces;
 
-	public PlanetRing(int level)
-	{
-		_level = level;
-		_pieces = new List<PlanetPiece>();
+    public PlanetRing(int level)
+    {
+        _level = level;
+        _pieces = new List<PlanetPiece>();
 
-		//total radius / base count / pow2 steps per level (1,2,4,8)->(a third of our cycle) 
-		_anglePerPieceOnThisLevel = 360f / GlobalConfig.PlanetBaseLevelSize / Mathf.Pow(2f, _level - 1);
+        //total radius / base count / pow2 steps per level (1,2,4,8)->(a third of our cycle) 
+        _anglePerPieceOnThisLevel = 360f / GlobalConfig.PlanetBaseLevelSize / Mathf.Pow(2f, _level - 1);
 
-		for (int i = 0; i < GlobalConfig.PlanetBaseLevelSize * Mathf.Pow(2, level - 1); i++)
-		{
-			_pieces.Add(new PlanetPiece(_level, _anglePerPieceOnThisLevel, i));
-		}
-	}
+        for (int i = 0; i < GlobalConfig.PlanetBaseLevelSize * Mathf.Pow(2, level - 1); i++)
+        {
+            _pieces.Add(new PlanetPiece(_level, _anglePerPieceOnThisLevel, i));
+        }
+    }
 
-	public PlanetPiece GetAtAngle(float angle)
-	{
-		return _pieces[CalculateIndex(angle)];
-	}
+    public PlanetPiece GetAtAngle(float angle)
+    {
+        return _pieces[CalculateIndex(angle)];
+    }
 
-	private int CalculateIndex(float angle)
-	{
-		return Mathf.FloorToInt(angle / _anglePerPieceOnThisLevel);
-	}
+    private int CalculateIndex(float angle)
+    {
+        return Mathf.FloorToInt(angle / _anglePerPieceOnThisLevel);
+    }
 }
 
 public class PlanetPiece
 {
-	public Elements element = Elements.NotSet;
-	public readonly int level;
-	public readonly float angleSize;
-	public readonly int indexOnRing;
+    public Elements element = Elements.NotSet;
+    public readonly int level;
+    public readonly float angleSize;
+    public readonly int indexOnRing;
 
-	public PlanetPiece(int level, float angleSize, int indexOnRing)
-	{
-		this.level = level;
-		this.angleSize = angleSize;
-		this.indexOnRing = indexOnRing;
-	}
+    public PlanetPiece(int level, float angleSize, int indexOnRing)
+    {
+        this.level = level;
+        this.angleSize = angleSize;
+        this.indexOnRing = indexOnRing;
+    }
 
-	public bool HasRightNeighbor()
-	{
-		return true;
-	}
+    public bool HasRightNeighbor()
+    {
+        return true;
+    }
 
-	public bool HasLeftNeighbor()
-	{
-		return true;
-	}
+    public bool HasLeftNeighbor()
+    {
+        return true;
+    }
 }
 
 public class TileSystem
 {
-	public readonly PlanetCycle planetCycle = new PlanetCycle();
+    public readonly PlanetCycle planetCycle = new PlanetCycle();
+    private TileSystemPainter _tileSystemPainter;
 
-	public void DoDrop(float angle, Elements element)
-	{
-		planetCycle.GetFirstFreeAtAngle(angle).element = element;
-	}
+    public TileSystem(TileSystemPainter tileSystemPainter)
+    {
+        _tileSystemPainter = tileSystemPainter;
+    }
+    public void DoDrop(float angle, Elements element)
+    {
+        Debug.Log("Draw new Tile " + element + " at Angle: " + angle);
+        PlanetPiece planetPiece = planetCycle.GetFirstFreeAtAngle(angle);
+        planetPiece.element = element;
+        
+        _tileSystemPainter.DrawTile(planetPiece);
+    }
 }
