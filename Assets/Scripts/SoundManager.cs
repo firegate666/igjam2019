50492065ee2a9500a7badac70610af54af69dacf
Marco Behnke <marco@behnke.biz>
2019-01-27 10:41:27 +0100

    sound manager and click sound

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource MenuClick;
+
+    public static SoundManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    public void PlayMenuClick()
+    {
+        MenuClick.Play();
+    }
+}
