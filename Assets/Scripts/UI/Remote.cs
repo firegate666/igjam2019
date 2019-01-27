6552ed2037a6af8c92825c7ac0bf535698216e59
Marco Behnke <marco@behnke.biz>
2019-01-27 13:52:22 +0100

    add moveable remote

--- Assets/Scripts/UI/Remote.cs
+++ Assets/Scripts/UI/Remote.cs
@@ -6,16 +6,24 @@ public class Remote : MonoBehaviour
 {
     public float MinX;
     public float MaxX;
+    public float MinY;
+    public float MaxY;
+    
     public float Speed;
 
     // Update is called once per frame
     void Update()
     {
         float x = Input.GetAxis("Xbox1Horizontal");
-        transform.Translate(x * Time.deltaTime * Speed, 0, 0);
+        float y = Input.GetAxis("Xbox1Vertical");
+        
+        transform.Translate(x * Time.deltaTime * Speed, y * Time.deltaTime * -Speed, 0);
 
         Vector3 newPosition = transform.position;
+        
         newPosition.x = Mathf.Clamp(newPosition.x, MinX, MaxX);        
+        newPosition.y = Mathf.Clamp(newPosition.y, MinY, MaxY);        
+        
         transform.position = newPosition;
     }
 }
