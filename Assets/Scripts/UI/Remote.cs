b28d6427ba51b9ff22f710f5170f87929ed51b1a
Marco Behnke <marco@behnke.biz>
2019-01-27 13:41:33 +0100

    moveable remote in ad screen

---
+++ Assets/Scripts/UI/Remote.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Remote : MonoBehaviour
+{
+    public float MinX;
+    public float MaxX;
+    public float Speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Xbox1Horizontal");
+        transform.Translate(x * Time.deltaTime * Speed, 0, 0);
+
+        Vector3 newPosition = transform.position;
+        newPosition.x = Mathf.Clamp(newPosition.x, MinX, MaxX);        
+        transform.position = newPosition;
+    }
+}
