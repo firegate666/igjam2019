dd5cf080f3e380d3836ac8b14823ea56feb34ae4
Marco Behnke <marco@behnke.biz>
2019-01-27 14:49:38 +0100

    hook up new sound in sound manager

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     public AudioSource MenuClick;
+    public AudioSource Fire;
+    public AudioSource Water;
+    public AudioSource Wood;
+    public AudioSource Impact;
 
     public static SoundManager Instance;
 
@@ -19,5 +23,25 @@ public class SoundManager : MonoBehaviour
     public void PlayMenuClick()
     {
         MenuClick.Play();
+    }
+    
+    public void PlayFire()
+    {
+        Fire.Play();
+    }
+    
+    public void PlayWater()
+    {
+        Water.Play();
+    }
+    
+    public void PlayImpact()
+    {
+        Impact.Play();
+    }
+    
+    public void PlayWood()
+    {
+        Wood.Play();
     }
 }
