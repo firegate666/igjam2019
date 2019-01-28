f802a8dcf82f9a309c4ef6c4d1266df32a1fee79
Marco Behnke <marco@behnke.biz>
2019-01-28 21:02:02 +0100

    handler for enable mobile controls

---
+++ Assets/Scripts/MobileManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileManager : MonoBehaviour
+{
+	public GameObject[] MobileOnlyGameObjects;
+	public bool DebugMode;
+
+	private bool _validationFinished;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		bool enableObjects = DebugMode;
+#if UNITY_IOS || UNITY_ANDROID
+		enableObjects = true;
+#endif
+		ValidateObjects(enableObjects);
+	}
+
+	void ValidateObjects(bool enableObjects)
+	{
+		foreach (GameObject go in MobileOnlyGameObjects)
+		{
+			go.SetActive(enableObjects);
+		}
+	}
+}
