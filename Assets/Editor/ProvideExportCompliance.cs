c41079b8f1c20e21e297e7953d35d79d7da7ade3
Marco Behnke <marco@behnke.biz>
2024-07-31 14:42:48 +0200

    fix export compliance default

---
+++ Assets/Editor/ProvideExportCompliance.cs
@@ -0,0 +1,36 @@
+﻿#if UNITY_IOS
+using UnityEngine;
+using UnityEditor.Callbacks;
+using UnityEditor;
+using UnityEditor.iOS.Xcode;
+using System.IO;
+ 
+public class ProvideExportCompliance
+{
+    [PostProcessBuild]
+    public static void ChangeXcodePlist(BuildTarget buildTarget, string pathToBuiltProject)
+    {
+        // Performs any post build processes that we need done
+        if( buildTarget == BuildTarget.iOS )
+        {
+            // PList modifications
+            {
+                // Get plist
+                string plistPath = pathToBuiltProject + "/Info.plist";
+                var plist = new PlistDocument();
+                plist.ReadFromString(File.ReadAllText(plistPath));
+ 
+                // Get root
+                var rootDict = plist.root;
+ 
+                // Add export compliance for TestFlight builds
+                var buildKeyExportCompliance = "ITSAppUsesNonExemptEncryption";
+                rootDict.SetString( buildKeyExportCompliance , "false" );
+               
+                // Write to file
+                File.WriteAllText( plistPath , plist.WriteToString() );
+            }
+        }
+    }
+}
+#endif
